916487792fb4d1d2d349a4cba02530ae31599313
Devin Plauche <devinplauche@gmail.com>
2021-02-26 22:16:16 -0700

    made main.java a loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     String input = args[0];
+    Scanner in = new Scanner(System.in);
     Calculator calculator = new Calculator();
+
+    System.out.println("Welcome to the calculator");
+    System.out.println("Enter a command:");
+    while (true) {
       if (input.equals("add")) {
         System.out.println(calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-      }
-      else if(input.equals("subtract")) {
+      } else if (input.equals("subtract")) {
         System.out.println(calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-      }
-      else if(input.equals("binary")) {
+      } else if (input.equals("binary")) {
         System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
-      }
-      else if(input.equals("fibonacci")) {
+      } else if (input.equals("fibonacci")) {
         System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
-      }
-      else if(input.equals("multiply")) {
+      } else if (input.equals("multiply")) {
         System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-      }
-      else if(input.equals("divide")) {
+      } else if (input.equals("divide")) {
         System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+      } else if (input.equals("quit")) {
+        return;
       }
+    }
     
 
   }
